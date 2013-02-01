45030419496b341cd883e7583c2c07214020ea71
Zheng Zhao <zhengzhaomail@gmail.com>
2013-02-01 18:09:20 +0800

    commit

---
+++ src/com/hisrv/android/sudokusolver/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hisrv.android.sudokusolver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hisrv.sudokusolver.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+}
