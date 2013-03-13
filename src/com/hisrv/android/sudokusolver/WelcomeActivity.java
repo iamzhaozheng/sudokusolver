12c7e43533d2c44172097b032ba81bd0fff6bf74
Zheng Zhao <zhengzhaomail@gmail.com>
2013-03-13 18:54:05 +0800

    commit

--- src/com/hisrv/android/sudokusolver/WelcomeActivity.java
+++ src/com/hisrv/android/sudokusolver/WelcomeActivity.java
@@ -2,8 +2,6 @@ package com.hisrv.android.sudokusolver;
 
 import java.io.IOException;
 
-import org.opencv.android.InstallCallbackInterface;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
@@ -12,7 +10,6 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
@@ -22,6 +19,7 @@ import android.graphics.Bitmap.Config;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.hisrv.sudokusolver.R;
 
@@ -34,22 +32,13 @@ public class WelcomeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
 		mImageView = (ImageView) findViewById(R.id.image);
-		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3,
-				getApplicationContext(), new LoaderCallbackInterface() {
-
-					public void onPackageInstall(int operation,
-							InstallCallbackInterface callback) {
-						// TODO Auto-generated method stub
-
-					}
-
-					public void onManagerConnected(int status) {
-						// TODO Auto-generated method stub
-						if (status == LoaderCallbackInterface.SUCCESS) {
-							process();
-						}
-					}
-				});
+		boolean ret = OpenCVLoader.initDebug();
+		if (ret) {
+			Toast.makeText(this, "loading cv first step pass.", Toast.LENGTH_LONG).show();
+			process();
+		} else {
+			Toast.makeText(this, "error on loading cv", Toast.LENGTH_LONG).show();
+		}
 	}
 
 	public void onBtnClick(View v) {
@@ -66,12 +55,11 @@ public class WelcomeActivity extends Activity {
 	private void process() {
 		try {
 			Mat mat = Utils.loadResource(getApplicationContext(),
-					R.raw.sudoku1, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+					R.raw.sudoku1, 0);
 			Mat outerBox = new Mat(mat.size(), CvType.CV_8UC1);
 			Imgproc.GaussianBlur(mat, mat, new Size(11, 11), 0);
 			Imgproc.adaptiveThreshold(mat, outerBox, 255,
-					Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,
-					5, 2);
+					Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 2);
 			Core.bitwise_not(outerBox, outerBox);
 			Mat kernel = new Mat(3, 3, CvType.CV_8UC1);
 			kernel.put(0, 0, new byte[] { 0, 1, 0, 1, 1, 1, 0, 1, 0 });
@@ -85,8 +73,8 @@ public class WelcomeActivity extends Activity {
 					outerBox.get(y, x, temp);
 					int b = temp[0] & 0xff;
 					if (b >= 128) {
-						int area = Imgproc.floodFill(outerBox, new Mat(), new Point(
-								x, y), new Scalar(64));
+						int area = Imgproc.floodFill(outerBox, new Mat(),
+								new Point(x, y), new Scalar(64));
 
 						if (area > max) {
 							maxPt = new Point(x, y);
@@ -101,14 +89,25 @@ public class WelcomeActivity extends Activity {
 					outerBox.get(y, x, temp);
 					int b = temp[0] & 0xff;
 					if (b == 64) {
-						Imgproc.floodFill(outerBox, new Mat(), new Point(
-								x, y), new Scalar(0));
+						Imgproc.floodFill(outerBox, new Mat(), new Point(x, y),
+								new Scalar(0));
 
 					}
 				}
 			}
 			Imgproc.erode(outerBox, outerBox, kernel);
-//			Imgproc.HoughLines(image, lines, rho, theta, threshold);
+			Mat lines = new Mat();
+			Imgproc.HoughLines(outerBox, lines, 1, 3.14159 / 180, 200);
+			for (int i = 0; i < lines.cols(); i++) {
+				double[] vec = lines.get(0, i);
+				double rho = vec[0];
+				double theta = vec[1];
+				double m = -1 / Math.tan(theta);
+				double c = rho / Math.sin(theta);
+				Point start = new Point(0, c);
+				Point end = new Point(outerBox.size().width, m * outerBox.size().width + c);
+				Core.line(outerBox, start, end, new Scalar(255, 0, 0), 3);
+			}
 			Bitmap bmp = Bitmap.createBitmap((int) mat.size().width,
 					(int) mat.size().height, Config.RGB_565);
 			Utils.matToBitmap(outerBox, bmp);
